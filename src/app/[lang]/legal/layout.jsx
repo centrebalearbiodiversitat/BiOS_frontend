803c4e050c94720d7eabef0a85a01bc740f251de
tomascbb <tomas.golomb@uib.cat>
2025-05-20 12:54:28 +0200

    improves legal layout pages

--- src/app/[lang]/legal/layout.jsx
+++ src/app/[lang]/legal/layout.jsx
@@ -1,6 +1,6 @@
 export default function Layout({children}) {
 	return (
-		<div className="flex justify-center mx-[4vw] md:mx-[10vw] py-[3vh] md:py-[8vh] mt-4 md:mt-10 border border-slate-200 shadow-md">
+		<div className="flex justify-center mx-[4vw] py-[3vh] md:py-[8vh]">
 			<article className="min-h-full max-w-[800px] mx-[8vw] xl:mx-0 text-pretty whitespace-pre-line flex flex-col gap-y-8">
 				{children}
 			</article>
